8bf52766dd5808fcb487aea4e53fd306d735a85d
Johnson <jhonsonj88@gmail.com>
2020-09-12 06:09:13 -0700

    created topic tab, imported @materialui styles

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,32 +1,74 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
+const useStyles = makeStyles((theme => ({
     root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      '& > *': {
-        margin: theme.spacing(1),
-        width: theme.spacing(16),
-        height: theme.spacing(16),
-      },
-    },
-  }),
-);
+        margin: "50px",
+        padding: theme.spacing(3, 2),
+        },
+         flex: {
+            display: 'flex',
+        },
+        topicWindow: {
+            width: '30%',
+            height: '300px',
+            borderRight: '1px solid red'
+        },
+         ChatWindow: {
+              width: '70%',
+              height: '300px'
+         },
+          chatBox: {
+               width: '85%'
+          },
+           button: {
+                width: '15%'
+           },
+       
+})));
+
+
 
 export default function Dashboard(){
 
     const classes = useStyles();
+
     return (
         <div>
-            <div className={classes.root}>
-              <Paper elevation={0} />
-              <Paper />
-              <Paper elevation={3} />
+            <Paper className={classes.root}>
+                <Typography variant='h4' component='h4'>
+                    Johnson Joseph Chat App
+                </Typography>
+                <Typography component='h5'>
+                    Topic Placeholder
+                </Typography>
+           <div className={classes.flex} >
+                <div className={classes.topicWindow}>
+                    <list>
+                        {
+                            ["topic"].map(topic => (
+                                  <ListItem key={topic} button>
+                                      <ListItemText primary={topic} />
+                                  </ListItem>
+                            ))
+                        }
+                    </list>
+                </div>
+                      
+                 <div className={classes.chatWindow}></div>
+           
+           </div>
+           <div className={classes.flex} >
+            <div className={classes.chatBox}></div>
+           
+           </div>
+        </Paper>
     </div>
-        </div>
-    )
+  )
 }
