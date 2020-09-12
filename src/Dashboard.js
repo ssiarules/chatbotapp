e60b5d0cff3749aa06b953d052d2551a8f6c5434
Johnson <jhonsonj88@gmail.com>
2020-09-11 17:49:42 -0700

    installed material-ui and created design for dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(1),
+        width: theme.spacing(16),
+        height: theme.spacing(16),
+      },
+    },
+  }),
+);
 
 export default function Dashboard(){
+
+    const classes = useStyles();
     return (
         <div>
-        
-        
+            <div className={classes.root}>
+              <Paper elevation={0} />
+              <Paper />
+              <Paper elevation={3} />
+    </div>
         </div>
     )
 }
